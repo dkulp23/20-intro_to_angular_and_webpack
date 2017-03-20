bc736b809b5f7141c54131197830265e4db289db
Dana Kulp <dkulp23@gmail.com>
2017-03-20 14:10:31 -0700

    Worked through docs to understand config file a little more.

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,25 @@
 'use strict';
 
 const HTMLPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  context: `${__dirname}/app`,
+  entry: './entry.js',
+  output: {
+    filename: 'bundle.js',
+    path: `${__dirname}/app/build`
+  },
+  plugins: [
+    new HTMLPlugin({
+      template: `${__dirname}/app.index.html`
+    })
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.scss$/,
+        loader: 'style!css!sass'
+      }
+    ]
+  }
+};
